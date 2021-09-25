f2f5f5816d391a0bc6afb965874305396a6865a5
Abdusalamov Dzhalil <abdusalamovdzhalil@gmail.com>
2021-09-25 20:31:16 +0300

    Пройден урок 20(NavLink)
    Добавлена новая компонента NavLink(import { NavLink } form 'react-route-dom')
    Компонента пишется вместо тега <a></a>. Href заменяется на to.
    С помощью атрибута activeClassName задаётся имя класса,
    для состояния активной ссылки.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import s from './Navbar.module.css';
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
     return (
         <nav className={s.nav}>
             <ul className={s.nav__list}>
-                <li><a href="/profile" className={s.nav__link}>Профиль</a></li>
-                <li><a href="/dialogs" className={s.nav__link}>Сообщения</a></li>
-                <li><a href="/news" className={s.nav__link}>Новости</a></li>
-                <li><a href="/settings" className={s.nav__link}>Настройки</a></li>
+                <li><NavLink activeClassName={s.active} to="/dialogs" className={s.nav__link}>Сообщения</NavLink></li>
+                <li><NavLink activeClassName={s.active} to="/profile" className={s.nav__link}>Профиль</NavLink></li>
+                <li><NavLink activeClassName={s.active} to="/news" className={s.nav__link}>Новости</NavLink></li>
+                <li><NavLink activeClassName={s.active} to="/settings" className={s.nav__link}>Настройки</NavLink></li>
             </ul>
         </nav>
     )
